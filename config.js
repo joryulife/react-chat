79fb86099282a88826dd5b20b7ed3f4f820e5025
joryulife <joryulife@gmail.com>
2021-07-01 06:15:20 +0900

    all

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDSuTVPZGKDbvkv8Anq0sHToCBlZOPOz8w",
+    authDomain: "react-chat-aef94.firebaseapp.com",
+    projectId: "react-chat-aef94",
+    databaseURL: "https://react-chat-aef94.PROJECT_ID.firebaseio.com",
+    storageBucket: "react-chat-aef94.appspot.com",
+    messagingSenderId: "663356730833",
+    appId: "1:663356730833:web:9a4f9856fdeaa21c3a44f0"
+  };
